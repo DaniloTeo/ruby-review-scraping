d6d0039c9dec529a6a2d029303cd774cd53f8d9b
Danilo Téo <danilo.teo@liven.tech>
2020-11-12 13:59:50 -0300

    feat: able to scrape reviews specifically. No other type of text

--- scrape_reviews.rb
+++ scrape_reviews.rb
@@ -47,7 +47,130 @@ def navigate_pages
     end
   end
 
-  print link_list
+  link_list
+end
+
+def linearize(matrix)
+  linearized_list = []
+  matrix.each do |list|
+    list.each do |link|
+      linearized_list.push(link)
+    end
+  end
+  linearized_list
+end
+
+def gather_paragraphs(body)
+  ps = []
+  body.each do |par|
+    ps.push(par.text)
+  end
+  ps
+end
+
+def clean_writers(writers)
+  if writers.scan(/&|,/) != []
+    writers.slice! 'Writers: '
+  else
+    writers.slice! 'Writer: '
+  end
+
+  writers
+end
+
+def clean_artists(artists)
+  artists.slice! 'Art: '
+  artists
+end
+
+def month_to_number(month_name)
+  month_number = ''
+  case month_name
+  when 'JANUARY'
+    month_number = '01'
+  when 'FEBRUARY'
+    month_number = '02'
+  when 'MARCH'
+    month_number = '03'
+  when 'APRIL'
+    month_number = '04'
+  when 'MAY'
+    month_number = '05'
+  when 'JUNE'
+    month_number = '06'
+  when 'JULY'
+    month_number = '07'
+  when 'AUGUST'
+    month_number = '08'
+  when 'SEPTEMBER'
+    month_number = '09'
+  when 'OCTOBER'
+    month_number = '10'
+  when 'NOVEMBER'
+    month_number = '11'
+  when 'DECEMBER'
+    month_number = '12'
+
+  end
+  month_number
+end
+
+def clean_timestamp(timestamp)
+  day = timestamp.scan(/\w ([0-9]{2})/)[0][0]
+  month = month_to_number(timestamp.scan(/[A-Z]+/)[0])
+  year = timestamp.scan(/, ([0-9]{4})/)[0][0]
+  "#{year}-#{month}-#{day}"
+end
+
+def clean_body(paragraphs)
+  body = ''
+  paragraphs.each do |par|
+    body += "#{par}\n" unless par == ''
+  end
+  body
+end
+
+def gather_review_data(review_link)
+  print_status("Gathering data from #{review_link}...")
+  @driver.get(review_link)
+  @wait.until { @driver.find_element(:css, 'footer.main-footer').displayed? }
+  header_list = @driver.find_elements(:css, 'h2')
+  title_issue = header_list[0].text
+  publisher = header_list[1].text
+  writers = header_list[2].text
+  artists = header_list[3].text
+
+  timestamp = @driver.find_element(:css, 'time.value-title').text
+
+  body = @driver.find_elements(:css, 'div.post-content > p')
+  paragraphs = gather_paragraphs(body)
+
+  begin
+    score = @driver.find_element(:css, 'div.overall > span > span').text
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    score = nil
+  end
+  score_number = nil
+  score_number = score.to_f unless score.nil?
+
+  {
+    title: title_issue,
+    publisher: publisher,
+    writers: clean_writers(writers),
+    artists: clean_artists(artists),
+    date: clean_timestamp(timestamp),
+    body: clean_body(paragraphs),
+    score: score_number
+  }
+end
+
+def gather_reviews(links)
+  reviews = []
+  links.each do |l|
+    review = gather_review_data(l)
+    reviews.push(review)
+  end
+  reviews
 end
 
 def scrape
@@ -60,17 +183,17 @@ def scrape
 
   # wait until the page's footer is displayed in browser
   @wait.until { @driver.find_element(:css, 'footer.main-footer').displayed? }
-  #   print_status('Page accessed...')
-
-  #   begin
-  #     print @driver.find_element(:css, 'a.next.page-numbers').displayed?
-  #   rescue Selenium::WebDriver::Error::NoSuchElementError => e
-  #     print e
-  #   end
+  print_status('Page accessed...')
 
   print_status('Preparing to navigate pages of review...')
-  navigate_pages
+  link_matrix = navigate_pages
 
+  links = linearize(link_matrix)
+  print_status('Visiting reviews and gathering review...')
+
+  reviews = gather_reviews(links)
+
+  File.write('./review.json', JSON.dump(reviews))
   # screen shot to assert we're where we want
   @driver.save_screenshot('./sp.png')
 end
